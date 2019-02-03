b57f1db197e7e6f08d3bc9cffc3edf40e830bf44
saaket123 <37738766+saaket123@users.noreply.github.com>
2019-02-03 18:25:21 +0530

    welcome msg added in feed

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -49,8 +49,8 @@ class FeedController extends Controller
            $id[] = $g->id;
        }
        if(!($user->remember_token)){
-       // $heading = ucfirst($user->fname) .", enjoy feed curated for you with ";
-            $heading = '';
+        $heading = ucfirst($user->fname) .", enjoy feed curated for you with ";
+            
     }
        else{
            $heading = '';
