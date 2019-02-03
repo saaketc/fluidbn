13895cafe5a136caede711c7604bc815a2c25f3f
saaket123 <37738766+saaket123@users.noreply.github.com>
2019-02-03 17:43:21 +0530

    music removed from show articles

--- resources/views/Article/show_article.blade.php
+++ resources/views/Article/show_article.blade.php
@@ -127,14 +127,15 @@
 
       <footer>
            <div class="footer-story">
-        <div class="w3-container">
+           {{--
+            <div class="w3-container">
        
     
       
           <audio controls style="margin-bottom:1rem;">
               <source src="/storage/music/f.mp3" type="audio/mpeg">
           </audio>
-        
+        --}}
         </div>
       </div>
            <button class="btn  btn-login {{$c}}" id="like"  style="margin-left:20px;margin-top:5px;" data-articleid="{{$article->id}}" type="submit">{{$like ? "Thanks" : "Wow"}}</button>
@@ -162,7 +163,7 @@
                    
                     <p class="lead">{!!wordwrap(str_limit($ra->content,50),20,"<br>\n",TRUE)!!}</p> @if($ra->views>0)<small class="views"> {{ '   '.$ra->views.' views'}}</small>@endif
                     <div class="">
-                      <img class="featurette-image img-fluid mx-auto  propic-small" src="/storage/profile_images/{{$ra->writtenBy->hasProfile->profile_image}}" alt=""> <small class="writer-small"><a href="{{route('profile',['user'=>$ra->writtenBy,'slug'=>str_slug($ra->writtenBy->fname." ".$ra->writtenBy->lname)])}}">{{ucfirst($ra->writtenBy->fname).' '. ucfirst($ra->writtenBy->lname)}}</a></small><div class=""><small class="margin writer-small">{{$ra->writtenBy->hasProfile->about }}</small></div>
+                      <img class="featurette-image img-fluid mx-auto  propic-small" src="/storage/profile_images/thumbnails/{{$ra->writtenBy->hasProfile->profile_image}}" alt=""> <small class="writer-small"><a href="{{route('profile',['user'=>$ra->writtenBy,'slug'=>str_slug($ra->writtenBy->fname." ".$ra->writtenBy->lname)])}}">{{ucfirst($ra->writtenBy->fname).' '. ucfirst($ra->writtenBy->lname)}}</a></small><div class=""><small class="margin writer-small">{{$ra->writtenBy->hasProfile->about }}</small></div>
                        </div>  
                   </div>
                 </div>
